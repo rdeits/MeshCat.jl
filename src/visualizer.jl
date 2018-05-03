752a8934a35c4a14295394372394a66b3ef4d030
Robin Deits <robin.deits@gmail.com>
2018-05-03 13:39:58 -0400

    import iframe fixes from WebIO upstream

--- src/visualizer.jl
+++ src/visualizer.jl
@@ -231,8 +231,23 @@ function iframe(dom)
             var frame = this.dom.querySelector("#ifr");
             var doc = frame.contentDocument
             var win = frame.contentWindow
+
+            // Determine if we're running on a Jupyter hosting service
+            // that requires a base URL when retrieving assets
+            var curMatch = 
+                window.location.href
+                .match(/(.*)\/notebooks\/.*\.ipynb/);
+            curMatch = curMatch ||
+                window.location.href
+                .match(/(.*)\/apps\/.*\.ipynb/);
+            if (curMatch) {
+                var base = doc.createElement("base");
+                base.setAttribute("href", curMatch[1] + '/');
+                doc.head.appendChild(base);
+            }
+
             var webio = doc.createElement("script")
-            webio.src = "/pkg/WebIO/webio/dist/bundle.js"
+            webio.src = "pkg/WebIO/webio/dist/bundle.js"
             var parent = window
 
             function resizeIframe() {
