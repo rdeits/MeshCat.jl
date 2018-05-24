ae1eeafbd72a547074ff9619f8b73680d12cd00f
Robin Deits <robin.deits@gmail.com>
2018-05-24 12:45:50 -0500

    clean up and add fallback

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,5 @@
+# Update the git submodule in `assets/meshcat`
+
 using Base.LibGit2: @check, GitRepo, CheckoutOptions, FetchOptions,
                     @kwdef, AbstractCredentials, RemoteCallbacks, credentials_cb
 
@@ -10,26 +12,23 @@ end
 
 const submodule_path = "assets/meshcat"
 
-# Update the git submodule in `assets/meshcat`
-repo = GitRepo(joinpath(@__DIR__, ".."))
-submodule_ptr_ptr = Ref{Ptr{Void}}(C_NULL)
-@check ccall((:git_submodule_lookup, :libgit2), Cint, (Ptr{Ptr{Void}}, Ptr{Void}, Cstring), submodule_ptr_ptr, repo.ptr, submodule_path)
+try
+    repo = GitRepo(joinpath(@__DIR__, ".."))
+    submodule_ptr_ptr = Ref{Ptr{Void}}(C_NULL)
 
-function submodule_update(submodule_ptr::Ptr{Void}, payload::Nullable{<:AbstractCredentials}=Nullable{AbstractCredentials}())
-    options = SubmoduleUpdateOptions(
-        fetch_opts=FetchOptions(callbacks=RemoteCallbacks(credentials_cb(), Ref(payload))),
-        )
-    # @check ccall((:git_submodule_init, :libgit2), Cint, (Ptr{Void}, Cint), submodule_ptr, 0)
+    @check ccall((:git_submodule_lookup, :libgit2), Cint, (Ptr{Ptr{Void}}, Ptr{Void}, Cstring), submodule_ptr_ptr, repo.ptr, submodule_path)
+    payload = Nullable{AbstractCredentials}()
+    options = SubmoduleUpdateOptions(fetch_opts=FetchOptions(callbacks=RemoteCallbacks(credentials_cb(), Ref(payload))))
     @check ccall((:git_submodule_update, :libgit2),
                  Cint,
                  (Ptr{Void}, Cint, Ptr{Void}),
-                 submodule_ptr, 1, Ref(options))
+                 submodule_ptr_ptr[], 1, Ref(options))
+
+    ccall((:git_submodule_free, :libgit2), Void, (Ptr{Void},), submodule_ptr_ptr[])
+catch e
+    warn("Using LibGit2 to update the meshcat submodule failed. Please report this issue at https://github.com/rdeits/MeshCat.jl/issues")
+    warn("LibGit2 error was: $e")
+    info("Falling back to command-line git")
+
+    run(`git submodule update --init --recursive`)
 end
-
-submodule_update(submodule_ptr_ptr[])
-
-ccall((:git_submodule_free, :libgit2), Void, (Ptr{Void},), submodule_ptr_ptr[])
-
-## Previous method, which relied on the user having the `git` executable
-## on their path:
-# run(`git submodule update --init --recursive`)
