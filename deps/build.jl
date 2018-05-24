938240671687734102a18076ae2cfb7c400dbbd3
Robin Deits <robin.deits@gmail.com>
2018-05-24 12:51:53 -0500

    comments

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,7 @@
 # Update the git submodule in `assets/meshcat`
+# This bulid process uses Julia's built-in support for
+# LibGit2 rather than relying on the user's command-line
+# git executable.
 
 using Base.LibGit2: @check, GitRepo, CheckoutOptions, FetchOptions,
                     @kwdef, AbstractCredentials, RemoteCallbacks, credentials_cb
@@ -16,14 +19,18 @@ try
     repo = GitRepo(joinpath(@__DIR__, ".."))
     submodule_ptr_ptr = Ref{Ptr{Void}}(C_NULL)
 
+    # int git_submodule_lookup(git_submodule **out, git_repository *repo, const char *name);
     @check ccall((:git_submodule_lookup, :libgit2), Cint, (Ptr{Ptr{Void}}, Ptr{Void}, Cstring), submodule_ptr_ptr, repo.ptr, submodule_path)
+
     payload = Nullable{AbstractCredentials}()
     options = SubmoduleUpdateOptions(fetch_opts=FetchOptions(callbacks=RemoteCallbacks(credentials_cb(), Ref(payload))))
+    # int git_submodule_update(git_submodule *submodule, int init, git_submodule_update_options *options);
     @check ccall((:git_submodule_update, :libgit2),
                  Cint,
                  (Ptr{Void}, Cint, Ptr{Void}),
                  submodule_ptr_ptr[], 1, Ref(options))
 
+    # void git_submodule_free(git_submodule *submodule);
     ccall((:git_submodule_free, :libgit2), Void, (Ptr{Void},), submodule_ptr_ptr[])
 catch e
     warn("Using LibGit2 to update the meshcat submodule failed. Please report this issue at https://github.com/rdeits/MeshCat.jl/issues")
