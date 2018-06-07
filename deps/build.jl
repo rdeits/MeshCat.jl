32541000a9d2a7417da08444e306c44772e6b1c5
Robin Deits <robin.deits@gmail.com>
2018-06-07 18:00:57 -0400

    stop using a git submodule to store the meshcat assets

--- deps/build.jl
+++ deps/build.jl
@@ -1,41 +1,45 @@
-# Update the git submodule in `assets/meshcat`
-# This bulid process uses Julia's built-in support for
-# LibGit2 rather than relying on the user's command-line
-# git executable.
+using Base.Filesystem
 
-using Base.LibGit2: @check, GitRepo, CheckoutOptions, FetchOptions,
-                    @kwdef, AbstractCredentials, RemoteCallbacks, credentials_cb
+include("../src/download_helpers.jl")
 
-@kwdef struct SubmoduleUpdateOptions
-    version::Cuint = 1
-    checkout_opts::CheckoutOptions = CheckoutOptions()
-    fetch_opts::FetchOptions
-    allow_fetch::Cint = 1
+const meshcat_sha = "18028760b377c178bc77ee61cf4b9de8d176d3c5"
+const meshcat_url = "https://github.com/rdeits/meshcat/archive/$meshcat_sha.zip"
+
+const assets_dir = normpath(joinpath(@__DIR__, "..", "assets"))
+const meshcat_dir = joinpath(assets_dir, "meshcat")
+const stamp_file = joinpath(assets_dir, "meshcat.stamp")
+
+function update_meshcat()
+    if !isdir(assets_dir)
+        mkpath(assets_dir)
+    end
+
+    if isdir(joinpath(meshcat_dir, ".git"))
+        info("Meshcat assets in $meshcat_dir have been cloned with git, so they will not be automatically downloaded. To force a re-download, delete or rename the directory $meshcat_dir")
+        return
+    end
+    if isfile(stamp_file)
+        stamped_sha = strip(open(readstring, stamp_file))
+        if stamped_sha == meshcat_sha 
+            return
+        else
+            info("Updating meshcat assets in $meshcat_dir from SHA $stamped_sha to $meshcat_sha")
+        end
+    end
+
+    if isdir(meshcat_dir)
+        rm(meshcat_dir; recursive=true)
+    end
+
+    mktempdir() do download_dir
+        download_path = joinpath(download_dir, "meshcat.zip")
+        run(DownloadHelpers.download_cmd(meshcat_url, download_path))
+        run(DownloadHelpers.unpack_cmd(download_path, download_dir, ".zip", nothing))
+        mv(joinpath(download_dir, "meshcat-$meshcat_sha"), meshcat_dir; remove_destination=true)
+    end
+    open(stamp_file, "w") do file
+        print(file, meshcat_sha)
+    end
 end
 
-const submodule_path = "assets/meshcat"
-
-try
-    repo = GitRepo(joinpath(@__DIR__, ".."))
-    submodule_ptr_ptr = Ref{Ptr{Void}}(C_NULL)
-
-    # int git_submodule_lookup(git_submodule **out, git_repository *repo, const char *name);
-    @check ccall((:git_submodule_lookup, :libgit2), Cint, (Ptr{Ptr{Void}}, Ptr{Void}, Cstring), submodule_ptr_ptr, repo.ptr, submodule_path)
-
-    payload = Nullable{AbstractCredentials}()
-    options = SubmoduleUpdateOptions(fetch_opts=FetchOptions(callbacks=RemoteCallbacks(credentials_cb(), Ref(payload))))
-    # int git_submodule_update(git_submodule *submodule, int init, git_submodule_update_options *options);
-    @check ccall((:git_submodule_update, :libgit2),
-                 Cint,
-                 (Ptr{Void}, Cint, Ptr{Void}),
-                 submodule_ptr_ptr[], 1, Ref(options))
-
-    # void git_submodule_free(git_submodule *submodule);
-    ccall((:git_submodule_free, :libgit2), Void, (Ptr{Void},), submodule_ptr_ptr[])
-catch e
-    warn("Using LibGit2 to update the meshcat submodule failed. Please report this issue at https://github.com/rdeits/MeshCat.jl/issues")
-    warn("LibGit2 error was: $e")
-    info("Falling back to command-line git")
-
-    run(`git submodule update --init --recursive`)
-end
+update_meshcat()
--- src/MeshCat.jl
+++ src/MeshCat.jl
@@ -16,6 +16,7 @@ using Requires: @require
 using Mux: page
 using WebIO
 using JSExpr
+using Base.Filesystem: rm
 
 import Compat
 import Base: delete!, length
@@ -64,6 +65,7 @@ include("trees.jl")
 using .SceneTrees
 include("geometry.jl")
 include("objects.jl")
+include("download_helpers.jl")
 include("animations.jl")
 include("commands.jl")
 include("abstract_visualizer.jl")
@@ -127,5 +129,24 @@ end
     end
 end
 
+function develop_meshcat_assets(skip_confirmation=false)
+    meshcat_dir = abspath(joinpath(@__DIR__, "..", "assets", "meshcat"))
+    if !skip_confirmation
+        println("CAUTION: This will delete all downloaded meshcat assets and replace them with a git clone.")
+        println("The following path will be overwritten:")
+        println(meshcat_dir)
+        println("To undo this operation, you will need to manually remove that directory and then run `Pkg.build(\"MeshCat\")`")
+        print("Proceed? (y/n) ")
+        choice = chomp(readline())
+        if isempty(choice) || lowercase(choice[1]) != 'y'
+            println("Canceled.")
+            return
+        end
+    end
+    println("Removing $meshcat_dir")
+    rm(meshcat_dir, force=true, recursive=true)
+    run(`git clone https://github.com/rdeits/meshcat $meshcat_dir`)
+    rm(joinpath(meshcat_dir, "..", "meshcat.stamp"))
+end
 
 end
