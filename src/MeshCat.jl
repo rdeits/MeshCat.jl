6a37dc74fa8bededb6ed8b704533f68d8f073af7
Robin Deits <robin.deits@gmail.com>
2018-06-15 00:46:38 -0400

    more noise

--- src/MeshCat.jl
+++ src/MeshCat.jl
@@ -2,7 +2,8 @@ __precompile__()
 
 module MeshCat
 
-using WebIO, GeometryTypes, CoordinateTransformations
+using WebIO
+using GeometryTypes, CoordinateTransformations
 using Rotations: rotation_between, Rotation
 using Colors: Color, Colorant, RGB, RGBA, alpha
 using StaticArrays: StaticVector, SVector, SDiagonal
