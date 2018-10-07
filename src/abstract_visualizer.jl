5281398e5fa2ba044b90f725473c13e26d234c7c
Ryan Elandt <ryan.elandt@gmail.com>
2018-10-07 12:48:13 -0400

    added setobject! for HomogenousMesh that uses the color in HMesh

--- src/abstract_visualizer.jl
+++ src/abstract_visualizer.jl
@@ -12,4 +12,10 @@ function setcontrol! end
 setobject!(vis::AbstractVisualizer, geom::GeometryLike) = setobject!(vis, Object(geom))
 setobject!(vis::AbstractVisualizer, geom::GeometryLike, material::AbstractMaterial) = setobject!(vis, Object(geom, material))
 
-
+function MeshCat.setobject!(vis::AbstractVisualizer, geom::HomogenousMesh)
+    if geom.color == nothing
+        return setobject!(vis, Object(geom))
+    else
+        return setobject!(vis, Object(geom, MeshLambertMaterial(color=geom.color)))
+    end
+end
