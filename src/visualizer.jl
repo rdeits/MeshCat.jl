a8b9d3180dbfecb1b3c577510f3d578511d83f72
Travis DePrato <travigd@umich.edu>
2019-04-05 23:03:02 -0400

    Change implementation of wait.

--- src/visualizer.jl
+++ src/visualizer.jl
@@ -112,11 +112,7 @@ function send(c::CoreVisualizer, cmd::AbstractCommand)
     nothing
 end
 
-function Base.wait(c::CoreVisualizer)
-    while isempty(c.scope.pool.connections)
-        sleep(0.25)
-    end
-end
+Base.wait(c::CoreVisualizer) = WebIO.ensure_connection(c.scope.pool)
 
 """
     vis = Visualizer()
