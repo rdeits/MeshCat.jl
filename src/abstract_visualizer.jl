49ecf81996b7087bc3b01f68f915a3d3a1ed8a0a
Twan Koolen <koolen.twan@gmail.com>
2019-04-06 19:34:33 -0400

    Use 'visible' property instead.

--- src/abstract_visualizer.jl
+++ src/abstract_visualizer.jl
@@ -11,4 +11,4 @@ function setcontrol! end
 # Convenient shortcuts
 setobject!(vis::AbstractVisualizer, geom::GeometryLike) = setobject!(vis, Object(geom))
 setobject!(vis::AbstractVisualizer, geom::GeometryLike, material::AbstractMaterial) = setobject!(vis, Object(geom, material))
-hide!(vis::AbstractVisualizer) = settransform!(vis, LinearMap(UniformScaling(false)))
+hide!(vis::AbstractVisualizer) = setprop!(vis, "visible", false)
