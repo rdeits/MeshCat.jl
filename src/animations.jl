888c2bc1d210fa1b2cf6fd0f2ac156d4098d0be1
Twan Koolen <koolen.twan@gmail.com>
2019-07-18 01:36:56 -0400

    Add mergesorted!.

--- src/animations.jl
+++ src/animations.jl
@@ -13,6 +13,35 @@ function Base.insert!(track::AnimationTrack, frame::Integer, value)
     return track
 end
 
+@inline function mergesorted!(dest::AbstractVector, a, b, lt=isless)
+    i = 1
+    it_a = iterate(a)
+    it_b = iterate(b)
+    while it_a !== nothing
+        val_a, state_a = it_a
+        if it_b === nothing
+            dest[i += 1] = val_a
+            copyto!(dest, i, Iterators.rest(a, state_a))
+            return dest
+        end
+        val_b, state_b = it_b
+        if lt(val_b, val_a)
+            dest[i] = val_b
+            it_b = iterate(b, state_b)
+        else
+            dest[i] = val_a
+            it_a = iterate(a, state_a)
+        end
+        i += 1
+    end
+    if it_b !== nothing
+        val_b, state_b = it_b
+        dest[i += 1] = val_b
+        copyto!(dest, i, Iterators.rest(b, state_b))
+    end
+    return dest
+end
+
 function Base.merge!(a::AnimationTrack, others::AnimationTrack...)
     for other in others
         @assert other.name == a.name
