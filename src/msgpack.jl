b113ffdbbdc926a7e6cdf3e1f4dc94dd5ce9cc6f
Twan Koolen <koolen.twan@gmail.com>
2019-10-06 20:27:59 -0400

    Fix MsgPack deprecation warning.

--- src/msgpack.jl
+++ src/msgpack.jl
@@ -4,7 +4,7 @@ extcode(::Type{Int32}) = 0x15
 extcode(::Type{UInt32}) = 0x16
 extcode(::Type{Float32}) = 0x17
 
-MsgPack.pack(io::IO, v::PackedVector) = pack(io, Ext(extcode(v),
+MsgPack.pack(io::IO, v::PackedVector) = pack(io, MsgPack.Extension(extcode(v),
     convert(Vector{UInt8},
         reshape(reinterpret(UInt8, v.data),
             (sizeof(v.data),)))))
