3c2d45eede96f2af7789e1fdb745d894ce915e36
Robin Deits <robin.deits@gmail.com>
2020-02-10 21:21:30 -0500

    clean up and implement basic WebIO rendering

--- src/MeshCat.jl
+++ src/MeshCat.jl
@@ -86,28 +86,10 @@ include("atframe.jl")
 include("arrow_visualizer.jl")
 include("ijulia.jl")
 include("servers.jl")
+include("assets.jl")
+include("integrations.jl")
 
 const VIEWER_ROOT = joinpath(@__DIR__, "..", "assets", "meshcat", "dist")
-
-function develop_meshcat_assets(skip_confirmation=false)
-    meshcat_dir = abspath(joinpath(@__DIR__, "..", "assets", "meshcat"))
-    if !skip_confirmation
-        println("CAUTION: This will delete all downloaded meshcat assets and replace them with a git clone.")
-        println("The following path will be overwritten:")
-        println(meshcat_dir)
-        println("To undo this operation, you will need to manually remove that directory and then run `Pkg.build(\"MeshCat\")`")
-        print("Proceed? (y/n) ")
-        choice = chomp(readline())
-        if isempty(choice) || lowercase(choice[1]) != 'y'
-            println("Canceled.")
-            return
-        end
-    end
-    println("Removing $meshcat_dir")
-    rm(meshcat_dir, force=true, recursive=true)
-    run(`git clone https://github.com/rdeits/meshcat $meshcat_dir`)
-    rm(joinpath(meshcat_dir, "..", "meshcat.stamp"))
-end
 
 function __init__()
     main_js = abspath(joinpath(VIEWER_ROOT, "main.min.js"))
@@ -116,16 +98,8 @@ function __init__()
         main.min.js not found at $main_js.
         Please build MeshCat using `import Pkg; Pkg.build("MeshCat")`""")
     end
-
-    @require Blink="ad839575-38b3-5650-b840-f874b8c74a25" begin
-        function Base.open(core::CoreVisualizer, w::Blink.AtomShell.Window)
-            # Ensure the window is ready
-            Blink.js(w, "ok")
-            # Set its contents
-            Blink.loadurl(w, url(core))
-            w
-        end
-    end
+    setup_integrations()
 end
+
 
 end
--- src/ijulia.jl
+++ src/ijulia.jl
@@ -1,22 +1,41 @@
-struct IJuliaCell
+struct DisplayedVisualizer
     core::CoreVisualizer
 end
 
+
+DisplayedVisualizer(vis::Visualizer) = DisplayedVisualizer(vis.core)
+
+url(c::DisplayedVisualizer) = url(c.core)
+
 """
-Render a MeshCat visualizer inline inside a Jupyter notebook cell.
+Render a MeshCat visualizer inline in Jupyter or Juno.
 
-The visualizer should show up automatically in your Jupyter cell output
-as long as this command is the *last* command in the input cell.
+If this is the last command in a Jupyter notebook cell, then the
+visualizer should show up automatically in the corresponding output
+cell.
+
+If this is run from the Juno console, then the visualizer should show
+up in the Juno plot pane.
 """
-IJuliaCell(v::Visualizer) = IJuliaCell(v.core)
+render(vis::Visualizer) = render(vis.core)
+render(core::CoreVisualizer) = DisplayedVisualizer(core)
 
-url(c::IJuliaCell) = url(c.core)
+@deprecate IJuliaCell(v::Visualizer) render(v)
 
-function Base.show(io::IO, ::MIME"text/html", frame::IJuliaCell)
+function Base.show(io::IO, ::MIME"text/html", frame::DisplayedVisualizer)
     wait_for_server(frame.core)
     print(io, """
     <div style="height: 500px; width: 100%; overflow-x: auto; overflow-y: hidden; resize: both">
     <iframe src="$(url(frame))" style="width: 100%; height: 100%; border: none"></iframe>
     </div>
 """)
+end
+
+function Base.show(io::IO, ::MIME"application/prs.juno.plotpane+html", d::DisplayedVisualizer)
+    wait_for_server(d.core)
+    print(io, """
+    <div style="height: 100%; width: 100%; overflow-x: auto; overflow-y: hidden; resize: both">
+    <iframe src="$(url(d.core))" style="width: 100%; height: 100%; border: none"></iframe>
+    </div>
+    """)
 end
