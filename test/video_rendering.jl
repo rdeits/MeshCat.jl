b914c55aaca8827b0f6f1d090481cf1c67ea3d05
Robin Deits <robin.deits@gmail.com>
2020-03-29 17:17:34 -0400

    Use new FFMPEG syntax, and require it to exist

--- test/video_rendering.jl
+++ test/video_rendering.jl
@@ -1,19 +1,11 @@
 using Base.Filesystem: rm
 
 @testset "video rendering" begin
-    has_ffmpeg = false
-    try
-        run(`ffmpeg -version`)
-        has_ffmpeg = true
-    catch e
-    end
-    if has_ffmpeg
-        mktempdir() do tmpdir
-            target = joinpath(tmpdir, "output.mp4")
-            MeshCat.convert_frames_to_video(
-                joinpath(@__DIR__, "data", "frames.tar"),
-                target)
-            @test isfile(target)
-        end
+    mktempdir() do tmpdir
+        target = joinpath(tmpdir, "output.mp4")
+        MeshCat.convert_frames_to_video(
+            joinpath(@__DIR__, "data", "frames.tar"),
+            target)
+        @test isfile(target)
     end
 end
