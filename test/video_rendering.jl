dd7c9035bc01f23ec7c7e99192602216388a0ce6
Robin Deits <robin.deits@gmail.com>
2020-03-29 17:23:57 -0400

    maintain compatibility with older julia versions

--- test/video_rendering.jl
+++ test/video_rendering.jl
@@ -1,11 +1,15 @@
 using Base.Filesystem: rm
 
 @testset "video rendering" begin
-    mktempdir() do tmpdir
-        target = joinpath(tmpdir, "output.mp4")
-        MeshCat.convert_frames_to_video(
-            joinpath(@__DIR__, "data", "frames.tar"),
-            target)
-        @test isfile(target)
+    if VERSION >= v"1.3-"
+        # Prior to Julia 1.3, we can't use the new FFMPEG_jll binaries
+        # which ensure that FFMPEG is available.
+        mktempdir() do tmpdir
+            target = joinpath(tmpdir, "output.mp4")
+            MeshCat.convert_frames_to_video(
+                joinpath(@__DIR__, "data", "frames.tar"),
+                target)
+            @test isfile(target)
+        end
     end
 end
