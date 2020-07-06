6bd1c7352efd641b77b26488734dbfdd90caa122
Henrique Ferrolho <henriqueferrolho@gmail.com>
2020-07-06 17:16:56 +0100

    Brings RotMatrix from Rotations.jl into scope

--- src/MeshCat.jl
+++ src/MeshCat.jl
@@ -1,7 +1,7 @@
 module MeshCat
 
 using GeometryTypes, CoordinateTransformations
-using Rotations: rotation_between, Rotation, UnitQuaternion
+using Rotations: rotation_between, Rotation, RotMatrix, UnitQuaternion
 using Colors: Color, Colorant, RGB, RGBA, alpha, hex
 using StaticArrays: StaticVector, SVector, SDiagonal, SMatrix
 using GeometryTypes: raw
