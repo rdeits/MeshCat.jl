11d3296c28a8d352636018483e4e35ee719d02a2
Rainer Rodrigues <rainerrodrigues16@gmail.com>
2026-03-26 08:36:51 +0000

    Allow passing kwargs to Electron window (fixes #122)

--- ext/ElectronExt.jl
+++ ext/ElectronExt.jl
@@ -3,8 +3,8 @@ module ElectronExt
 import MeshCat
 import Electron
 
-function Base.open(core::MeshCat.CoreVisualizer, w::Electron.Application)
-    Electron.Window(w, Electron.URI(MeshCat.url(core)))
+function Base.open(core::MeshCat.CoreVisualizer, w::Electron.Application; kwargs...)
+    Electron.Window(w, Electron.URI(MeshCat.url(core)); kwargs...)
     w
 end
 
